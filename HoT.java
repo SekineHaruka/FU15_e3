6a6e9511a7b50fc600d03b918865c43a37fa9d70
SekineHaruka <sakamotoon@icloud.com>
2024-07-02 14:49:24 +0900

    User name added

--- HoT.java
+++ HoT.java
@@ -2,6 +2,11 @@ import java.util.Random;
 
 public class HoT{
     public static void main(String[] args) {
+        System.out.println("Who are you?");
+        Scanner sc = new Scanner(System.in);
+        String username = sc.next();
+        System.out.println("Hello,"+username+"!");
+        
         String[] hot = {"Heads", "Tails"};
         Random rdm = new Random();
         int heads=0, tails=0;

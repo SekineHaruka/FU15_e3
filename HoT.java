6793d84300f0ec251eb5505fd6ad3c91e945f169
SekineHaruka <sakamotoon@icloud.com>
2024-07-02 14:47:39 +0900

    HoT: initial revision

---
+++ HoT.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class HoT{
+    public static void main(String[] args) {
+        String[] hot = {"Heads", "Tails"};
+        Random rdm = new Random();
+        int heads=0, tails=0;
+        for(int i=1;i<=5;i++){
+            int j;
+            String value = hot[j=rdm.nextInt(2)];
+            System.out.println("Round "+i+":"+value);
+            if (j==0) heads++;
+            if (j==1) tails++;
+        }
+        System.out.println("Heads: "+heads+", Tails: "+tails);
+    }
+}
